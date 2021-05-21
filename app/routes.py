90633d05d241f62f5404ec850a59b514a2250c19
Стефанович Владислав <vladisla.stefanovich@softclub.by>
2021-05-21 17:46:20 +0300

    Completed lesson 2.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    # return 'Hello World!'
-    return "Привет Мир!"
-
+    user = {'username': 'vlad'}
+    title = 'Home'
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title=title, user=user, posts=posts)

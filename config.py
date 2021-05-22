2c8cd55261e5605c36fd4c4f1e41f68d91f1844a
gadget <road.runner.grach@gmail.com>
2021-05-22 13:22:15 +0300

    Added sqlalchemy and flask_migrate in project. Added folder migrations in project. Modified code for work with new modules.

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
                  'ouyrg$&7p-1)f@76vdqr9@da$figdarq292^45eh7%85(je**y'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
